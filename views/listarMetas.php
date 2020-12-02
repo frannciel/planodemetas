990206bb4050ae869bdcae77c5e8e20b8882a06c
Franciel <frannciel.edu@gmail.com>
2020-12-02 00:47:22 -0300

    Update listarMetas.php
    Atualizando para pmi 2021

--- views/listarMetas.php
+++ views/listarMetas.php
@@ -3,7 +3,7 @@
 	<div class="col-lg-10">
 		<div class="panel panel-default sombra">
 			<div class="panel-heading">
-				<center><h1>PLANO DE METAS INSTITUCIONAL 2020</h1></center>
+				<center><h1>PLANO DE METAS INSTITUCIONAL 2021</h1></center>
 			</div><!-- painel-heading -->
 
 			<div class="panel-body">
